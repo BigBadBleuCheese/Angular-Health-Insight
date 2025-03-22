22f287b4fef10471276b47f7aa99c7a587505055
Daniel Henry <daniel.henry@gmail.com>
2025-03-22 18:59:46 -0400

    move themes out into separate scss; support system light mode / dark mode fully; navigation init; user name in session service; route to login if not logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { SessionService } from './session.service';
 import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   imports: [
-    FooterComponent,
     RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    AsyncPipe,
+    FooterComponent,
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Angular Health Insight';
+export class AppComponent implements OnInit {
+  private breakpointObserver = inject(BreakpointObserver);
+  title: string = 'Angular Health Insight';
+  userName: string = '';
+
+  constructor(private router: Router, private sessionService: SessionService) {}
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+  
+  isUserLoggedIn() {
+    return this.userName.trim() !== '';
+  }
+
+  ngOnInit(): void {
+    this.userName = this.sessionService.getUserName();
+    this.sessionService.onUserNameChanged().subscribe(userName => this.userName = userName);
+    if (this.userName === '') {
+      this.router.navigate(['/login']);
+      return;
+    }
+  }
 }
--- src/app/session.service.ts
+++ src/app/session.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
+  private userName: string = '';
+  private userNameSubject = new Subject<any>();
 
   constructor() { }
+
+  getUserName(): string {
+    return this.userName;
+  }
+
+  logIn(userName: string): void {
+    if (this.userName !== '') {
+      throw new Error('A user is already logged in.');
+    }
+    if (userName.trim() === '') {
+      throw new Error('The provided user name was blank.');
+    }
+    this.userName = userName;
+    this.userNameSubject.next(this.userName);
+  }
+
+  logOut(): void {
+    if (this.userName === '') {
+      throw new Error('There is no user logged in.');
+    }
+    this.userName = '';
+    this.userNameSubject.next(this.userName);
+  }
+
+  onUserNameChanged(): Observable<any> {
+    return this.userNameSubject.asObservable();
+  }
 }
