d239740af061297ef5be16ebe8fc4f111258b296
Daniel Henry <daniel.henry@gmail.com>
2025-03-22 21:43:10 -0400

    summary.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { SummaryComponent } from './summary/summary.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginFormComponent }
+    { path: '', component: SummaryComponent },
+    { path: 'login', component: LoginFormComponent },
 ];
